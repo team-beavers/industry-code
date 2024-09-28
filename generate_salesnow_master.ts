7f1cfaa5ed67c0e08625ccba790f82aba546d933
R-Otsuka <yakugozy@gmail.com>
2024-09-29 08:30:50 +0900

    fix

--- generate_salesnow_master.ts
+++ generate_salesnow_master.ts
@@ -1,6 +1,3 @@
-// jsic分類項目表(第12回改訂)から[大分類、中分類、小分類、最分類]4つのmasterデータを出力する。
-// jsicとtsrの差分を考慮[https://www.tsr-net.co.jp/service/detail/pdf/code_book.pdf]
-
 import fs from 'fs';
 import _ from 'lodash';
 import { parse } from 'csv-parse/sync';
@@ -10,10 +7,27 @@ const headerMap = {
   division: ['id', 'section_id', 'name'],
 } as const;
 
+type HeaderMap = typeof headerMap;
 
-const writeCSV = (filePath: string, data: any[], type: keyof typeof headerMap) => {
+type DataType = {
+  [K in keyof HeaderMap]: {
+    [Field in HeaderMap[K][number]]: string | number;
+  };
+};
+
+
+const writeCSV = <T extends keyof HeaderMap>(
+  filePath: string,
+  data: DataType[T][],
+  type: T
+) => {
   const header = `${headerMap[type]}\n`;
-  const rows = _.map(data, row => _.map(row, item => `"${item}"`).join(',')).join('\n');
+  const rows = _.map(data, (row) => {
+    // nameは・周りやる
+    return _.map(headerMap[type], (key) => {
+      return `"${row[key]}"`
+    }).join(',');
+  }).join('\n');
   fs.writeFileSync(filePath, header + rows);
 };
 
@@ -23,17 +37,15 @@ const writeCSV = (filePath: string, data: any[], type: keyof typeof headerMap) =
   const fileData = fs.readFileSync(filePath, 'utf-8');
   const salesnowMasterData = parse(fileData, { quote: '"', ltrim: true, rtrim: true, delimiter: ',' });
 
-  const sectionData: any[] = [];
-  const divisionData: any[] = [];
+  const sectionData: DataType['section'][] = [];
+  const divisionData: DataType['division'][] = [];
 
   let sectionIndex: number = 1;
   const sectionMap: { [key: string]: number } = {};
 
   for (let i = 1; i < salesnowMasterData.length; i++) {
     const row = salesnowMasterData[i];
-    // 大分類、中分類
     const [sectionName, divisionName] = row;
-    // 大分類の処理
     if (!sectionMap[sectionName]) {
       sectionData.push({
         id: sectionIndex,
@@ -44,16 +56,12 @@ const writeCSV = (filePath: string, data: any[], type: keyof typeof headerMap) =
     }
 
     const sectionId = sectionMap[sectionName];
-    // 中分類の処理
     divisionData.push({
       id: i,
       section_id: sectionId,
       name: divisionName
     });
   }
-
-  console.log(sectionData, 'sectionData');
-  console.log(divisionData, 'divisionData');
 
   writeCSV('./resource/salesnow/salesnow_section_master.csv', sectionData, 'section');
   writeCSV('./resource/salesnow/salesnow_division_master.csv', divisionData, 'division');
